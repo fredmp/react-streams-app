54da8ff1384220486d9409153355bc191b1e1fc0
Frederico Martins Porto <martinsporto@gmail.com>
2019-02-06 11:50:07 -0200

    Finish react router configuration and add CRUD boilerplate

--- src/components/App.js
+++ src/components/App.js
@@ -1,46 +1,27 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-
-const Home = () => {
-  return (
-    <div>
-      Home
-      <Link to="/1">1</Link>
-      <Link to="/2">2</Link>
-    </div>
-  );
-};
-
-const PageOne = () => {
-  return (
-    <div>
-      Page one
-      <Link to="/">Home</Link>
-      <Link to="/2">2</Link>
-    </div>
-  );
-};
-
-const PageTwo = () => {
-  return (
-    <div>
-      Page two
-      <Link to="/">Home</Link>
-      <Link to="/1">1</Link>
-    </div>
-  );
-};
+import Header from './Header';
+import StreamCreate from './streams/StreamCreate';
+import StreamShow from "./streams/StreamShow";
+import StreamList from "./streams/StreamList";
+import StreamEdit from "./streams/StreamEdit";
+import StreamDelete from "./streams/StreamDelete";
 
 const App = () => {
   return (
-    <div>
+    <div className="ui container">
       <Router>
-        <Switch>
-          <Route path="/" component={Home} exact />
-          <Route path="/1" component={PageOne} />
-          <Route path="/2" component={PageTwo} />
-        </Switch>
+        <div>
+          <Header />
+          <Switch>
+            <Route path="/" component={StreamList} exact />
+            <Route path="/show" component={StreamShow} />
+            <Route path="/new" component={StreamCreate} />
+            <Route path="/edit" component={StreamEdit} />
+            <Route path="/delete" component={StreamDelete} />
+          </Switch>
+        </div>
       </Router>
     </div>
   );
