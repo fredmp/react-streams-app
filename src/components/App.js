ba29bef308927b1aedee7b1bc04f61e7bb364366
Frederico Martins Porto <martinsporto@gmail.com>
2019-02-06 11:32:39 -0200

    Setup react router

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,49 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+
+
+const Home = () => {
+  return (
+    <div>
+      Home
+      <Link to="/1">1</Link>
+      <Link to="/2">2</Link>
+    </div>
+  );
+};
+
+const PageOne = () => {
+  return (
+    <div>
+      Page one
+      <Link to="/">Home</Link>
+      <Link to="/2">2</Link>
+    </div>
+  );
+};
+
+const PageTwo = () => {
+  return (
+    <div>
+      Page two
+      <Link to="/">Home</Link>
+      <Link to="/1">1</Link>
+    </div>
+  );
+};
 
 const App = () => {
-  return <div>App</div>
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/1" component={PageOne} />
+          <Route path="/2" component={PageTwo} />
+        </Switch>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
